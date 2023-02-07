75e475665e7836836b9d1efaa5aa1a7f2dc9fb44
Ali Jahankah <aral2@yahoo.com>
2023-02-07 17:34:14 +0000

    setter | Getter | static

--- dist/app.js
+++ dist/app.js
@@ -4,10 +4,25 @@ class Student {
         this.id = id;
         this.name = name;
         this.class = [];
+        this.item = name;
     }
     describe() {
         console.log(`Mr/Ms ${this.name}`);
         // this.name = "Reza"; //Read only!!!!!
+    }
+    static showLog(value) {
+        console.log(value);
+    }
+    get gettingPrivet() {
+        return this.item;
+    }
+    set settingPrivetItem(value) {
+        if (value) {
+            console.log(`${value} exist`);
+            this.item = "Item public";
+            console.log(this.item);
+        }
+        new Error("There is an error");
     }
 }
 class Person extends Student {
@@ -22,11 +37,13 @@ class Person extends Student {
     }
 }
 const newStudent = new Student(5, "Hasan");
+newStudent.settingPrivetItem = "NewStter";
 const newPerson = new Person(666, ["First report..."]);
 newPerson.describe();
 newPerson.showReports();
 console.log(newStudent);
 console.log(newPerson);
+Student.showLog("Hi");
 // Student.describe();
 // newStudent.describe();
 // const secondOne = {name:"Reza"  id: 8, describe: newStudent.describe };
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,27 @@
 class Student {
+  private item: string;
+
   protected class: string[] = [];
-  constructor(private readonly id: number, public name: string) {}
+  constructor(private readonly id: number, public name: string) {
+    this.item = name;
+  }
   describe(this: Student) {
     console.log(`Mr/Ms ${this.name}`);
     // this.name = "Reza"; //Read only!!!!!
+  }
+  static showLog(value: number | string) {
+    console.log(value);
+  }
+  get gettingPrivet() {
+    return this.item;
+  }
+  set settingPrivetItem(value: string) {
+    if (value) {
+      console.log(`${value} exist`);
+      this.item = "Item public";
+      console.log(this.item);
+    }
+    new Error("There is an error");
   }
 }
 
@@ -19,12 +37,13 @@ class Person extends Student {
 }
 
 const newStudent = new Student(5, "Hasan");
+newStudent.settingPrivetItem = "NewStter";
 const newPerson = new Person(666, ["First report..."]);
 newPerson.describe();
 newPerson.showReports();
 console.log(newStudent);
 console.log(newPerson);
-
+Student.showLog("Hi");
 // Student.describe();
 // newStudent.describe();
 // const secondOne = {name:"Reza"  id: 8, describe: newStudent.describe };
