089b6e0e0226e0ef01a6c49b0bf3ebcd37be9a8b
Ali Jahankah <aral2@yahoo.com>
2023-02-09 11:52:44 +0000

    interfaces finished

--- dist/interfaces.js
+++ dist/interfaces.js
@@ -1,13 +1,28 @@
 "use strict";
+let customFunction;
+customFunction = (val) => {
+    if (val) {
+        console.log("Custom function" + val);
+    }
+    else {
+        console.log("Custom - no value");
+    }
+};
 class Student2 {
     constructor(n) {
         this.age = 26;
         this.name = n;
     }
     greet(value) {
-        console.log(this.name + value);
+        if (this.name) {
+            console.log(this.name + value);
+        }
+        else {
+            console.log(value + " There is no name");
+        }
     }
 }
-const student2 = new Student2("Ali");
+const student2 = new Student2("");
 student2.greet(" is Human");
+customFunction("X*s");
 //# sourceMappingURL=interfaces.js.map
--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,5 +1,16 @@
+interface custumFn {
+  (val?: string): void;
+}
+let customFunction: custumFn;
+customFunction = (val?: string) => {
+  if (val) {
+    console.log("Custom function" + val);
+  } else {
+    console.log("Custom - no value");
+  }
+};
 interface Person1 {
-  readonly name: string;
+  readonly name?: string;
   age: number;
 }
 interface Greeting extends Person1 {
@@ -8,14 +19,19 @@ interface Greeting extends Person1 {
 
 class Student2 implements Greeting {
   age = 26;
-  name: string;
-  constructor(n: string) {
+  name?: string;
+  constructor(n?: string) {
     this.name = n;
   }
   greet(value: string): void {
-    console.log(this.name + value);
+    if (this.name) {
+      console.log(this.name + value);
+    } else {
+      console.log(value + " There is no name");
+    }
   }
 }
-const student2 = new Student2("Ali");
+const student2 = new Student2("");
 
 student2.greet(" is Human");
+customFunction("X*s");
