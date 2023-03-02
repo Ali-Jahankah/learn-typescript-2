a5a84af1e622d39b0cea2f34967a5cc44d305e96
Ali Jahankah <aral2@yahoo.com>
2023-03-02 12:31:53 +0000

    Finishing basic TypeScript

--- dist/app.js
+++ dist/app.js
@@ -1,123 +1,147 @@
 "use strict";
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-var __param = (this && this.__param) || function (paramIndex, decorator) {
-    return function (target, key) { decorator(target, key, paramIndex); }
-};
 //Decorators & Factory decorators =>function | Capital letter | Using @ before name | Written On the top of Classes
-const Logger = (constructor) => {
-    console.log("This is a Looger function");
-    console.log(constructor);
-};
-const SecondLogger = (constructor) => {
-    console.log("This is the second Decorator function");
-};
-const DecFactory = (st, id) => {
-    console.log("Outside deco fact");
-    return (orgConstructor) => {
-        return class extends orgConstructor {
-            constructor(..._) {
-                super();
-                if (st && id) {
-                    const h1Tag = document.getElementById(id);
-                    h1Tag.innerHTML = this.name;
-                }
-                // console.log(constructor);
-                console.log("inside Deco factory" + st);
-            }
-        };
-    };
-};
-//Property Decorator | Accessor decorators | Method decorators | Parameter decorators
-let Student = class Student {
-    constructor() {
-        this.name = "Ali";
-        console.log("Creating student...");
-    }
-};
-Student = __decorate([
-    DecFactory("<span>Ali</span>", "test"),
-    SecondLogger,
-    Logger
-], Student);
-const newStudent = new Student();
-console.log(newStudent);
-const Log1 = (target, name) => {
-    console.log("Property decorator...");
-    console.log(target);
-    console.log(name);
-};
-const Log2 = (target, name, decorator) => {
-    console.log("accessor decorator");
-    console.log(target);
-    console.log(name);
-    console.log(decorator);
-};
-const Log3 = (target, name, decorator) => {
-    console.log("Method decorator.-.-.-.-.-.-.-");
-    console.log(target);
-    console.log(name);
-    console.log(decorator);
-};
-const Log4 = (target, name, position) => {
-    console.log("Parameter decorator");
-    console.log(target);
-    console.log(name);
-    console.log(position);
-};
-class Product {
-    constructor(name, price) {
-        this.name = name;
-        this._price = price;
-    }
-    set price(val) {
-        if (val > 0 && val) {
-            this._price = val;
-        }
-        else {
-            throw new Error("Insert a posetive value!");
-        }
-    }
-    createTax(tax) {
-        return this._price + tax;
-    }
-}
-__decorate([
-    Log1
-], Product.prototype, "name", void 0);
-__decorate([
-    Log2
-], Product.prototype, "price", null);
-__decorate([
-    Log3,
-    __param(0, Log4)
-], Product.prototype, "createTax", null);
-const AutoBinder = (_, _2, descriptor) => {
-    const method = descriptor.value;
-    const newDescriptor = {
-        get() {
-            const boundFn = method.bind(this);
-            return boundFn;
-        },
-    };
-    return newDescriptor;
-};
-class Printer {
-    constructor() {
-        this.message = "It works";
-    }
-    showPrint() {
-        console.log(this.message);
-    }
-}
-__decorate([
-    AutoBinder
-], Printer.prototype, "showPrint", null);
-const newPrint = new Printer();
-const btn = document.querySelector("button");
-btn.addEventListener("click", newPrint.showPrint.bind(newPrint));
+// import {
+//   Validate,
+//   validateOrReject,
+//   IsInt,
+//   Length,
+//   validate,
+//   Min,
+//   Max,
+// } from "class-validator";
+// const cv = require("class-validator");
+// const Logger = (constructor: Function) => {
+//   console.log("This is a Looger function");
+//   console.log(constructor);
+// };
+// const SecondLogger = (constructor: Function) => {
+//   console.log("This is the second Decorator function");
+// };
+// const DecFactory = (st: string, id: string) => {
+//   console.log("Outside deco fact");
+//   return <T extends { new (...args: any[]): { name: string } }>(
+//     orgConstructor: T
+//   ) => {
+//     return class extends orgConstructor {
+//       constructor(..._: any[]) {
+//         super();
+//         if (st && id) {
+//           const h1Tag = document.getElementById(id)!;
+//           h1Tag.innerHTML = this.name;
+//         }
+//         // console.log(constructor);
+//         console.log("inside Deco factory" + st);
+//       }
+//     };
+//   };
+// };
+// //Property Decorator | Accessor decorators | Method decorators | Parameter decorators
+// @DecFactory("<span>Ali</span>", "test")
+// @SecondLogger
+// @Logger
+// class Student {
+//   name: string;
+//   constructor(name: string) {
+//     this.name = name;
+//     console.log("Creating student...");
+//   }
+// }
+// const newStudent = new Student("Hasan");
+// console.log(newStudent);
+// const Log1 = (target: any, name: string | Symbol) => {
+//   console.log("Property decorator...");
+//   console.log(target);
+//   console.log(name);
+// };
+// const Log2 = (target: any, name: string, decorator: PropertyDescriptor) => {
+//   console.log("accessor decorator");
+//   console.log(target);
+//   console.log(name);
+//   console.log(decorator);
+// };
+// const Log3 = (
+//   target: any,
+//   name: string | Symbol,
+//   decorator: PropertyDescriptor
+// ) => {
+//   console.log("Method decorator.-.-.-.-.-.-.-");
+//   console.log(target);
+//   console.log(name);
+//   console.log(decorator);
+// };
+// const Log4 = (target: any, name: string | Symbol, position: number) => {
+//   console.log("Parameter decorator");
+//   console.log(target);
+//   console.log(name);
+//   console.log(position);
+// };
+// class Product {
+//   @Log1
+//   name: string;
+//   private _price: number;
+//   constructor(name: string, price: number) {
+//     this.name = name;
+//     this._price = price;
+//   }
+//   @Log2
+//   set price(val: number) {
+//     if (val > 0 && val) {
+//       this._price = val;
+//     } else {
+//       throw new Error("Insert a posetive value!");
+//     }
+//   }
+//   @Log3
+//   createTax(@Log4 tax: number): number {
+//     return this._price + tax;
+//   }
+// }
+// const AutoBinder = (_: any, _2: string, descriptor: PropertyDescriptor) => {
+//   const method = descriptor.value;
+//   const newDescriptor: PropertyDescriptor = {
+//     get() {
+//       const boundFn = method.bind(this);
+//       return boundFn;
+//     },
+//   };
+//   return newDescriptor;
+// };
+// class Printer {
+//   message = "It works";
+//   @AutoBinder
+//   showPrint() {
+//     console.log(this.message);
+//   }
+// }
+// const newPrint = new Printer();
+// const btn = document.querySelector("button")! as HTMLButtonElement;
+// btn.addEventListener("click", newPrint.showPrint.bind(newPrint));
+// class Course {
+//   @Length(1, 10)
+//   title: string;
+//   @Min(2)
+//   @Max(8)
+//   task: number;
+//   constructor(title: string, task: number) {
+//     this.title = title;
+//     this.task = task;
+//   }
+// }
+// const form = document.getElementById("form")! as HTMLFormElement;
+// const title = document.getElementById("title")! as HTMLInputElement;
+// const task = document.getElementById("task")! as HTMLInputElement;
+// const validation = (course: {}) => {
+//   validate(course).then((err: any) => {
+//     if (err.length > 0) {
+//       console.log("Validation error", err);
+//     } else {
+//       console.log("Course created");
+//     }
+//   });
+// };
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   const newCourse = new Course(title.value, +task.value);
+//   validation(newCourse);
+// });
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,113 +1,154 @@
 //Decorators & Factory decorators =>function | Capital letter | Using @ before name | Written On the top of Classes
-import { Validate, validateOrReject, IsInt, Length } from "class-validator";
-const Logger = (constructor: Function) => {
-  console.log("This is a Looger function");
-  console.log(constructor);
-};
-const SecondLogger = (constructor: Function) => {
-  console.log("This is the second Decorator function");
-};
+// import {
+//   Validate,
+//   validateOrReject,
+//   IsInt,
+//   Length,
+//   validate,
+//   Min,
+//   Max,
+// } from "class-validator";
+// const cv = require("class-validator");
 
-const DecFactory = (st: string, id: string) => {
-  console.log("Outside deco fact");
-  return <T extends { new (...args: any[]): { name: string } }>(
-    orgConstructor: T
-  ) => {
-    return class extends orgConstructor {
-      constructor(..._: any[]) {
-        super();
-        if (st && id) {
-          const h1Tag = document.getElementById(id)!;
-          h1Tag.innerHTML = this.name;
-        }
-        // console.log(constructor);
-        console.log("inside Deco factory" + st);
-      }
-    };
-  };
-};
-//Property Decorator | Accessor decorators | Method decorators | Parameter decorators
-@DecFactory("<span>Ali</span>", "test")
-@SecondLogger
-@Logger
-class Student {
-  name: string;
-  constructor(name: string) {
-    this.name = name;
-    console.log("Creating student...");
-  }
-}
-const newStudent = new Student("Hasan");
-console.log(newStudent);
+// const Logger = (constructor: Function) => {
+//   console.log("This is a Looger function");
+//   console.log(constructor);
+// };
+// const SecondLogger = (constructor: Function) => {
+//   console.log("This is the second Decorator function");
+// };
 
-const Log1 = (target: any, name: string | Symbol) => {
-  console.log("Property decorator...");
-  console.log(target);
-  console.log(name);
-};
-const Log2 = (target: any, name: string, decorator: PropertyDescriptor) => {
-  console.log("accessor decorator");
-  console.log(target);
-  console.log(name);
-  console.log(decorator);
-};
-const Log3 = (
-  target: any,
-  name: string | Symbol,
-  decorator: PropertyDescriptor
-) => {
-  console.log("Method decorator.-.-.-.-.-.-.-");
-  console.log(target);
-  console.log(name);
-  console.log(decorator);
-};
-const Log4 = (target: any, name: string | Symbol, position: number) => {
-  console.log("Parameter decorator");
-  console.log(target);
-  console.log(name);
-  console.log(position);
-};
+// const DecFactory = (st: string, id: string) => {
+//   console.log("Outside deco fact");
+//   return <T extends { new (...args: any[]): { name: string } }>(
+//     orgConstructor: T
+//   ) => {
+//     return class extends orgConstructor {
+//       constructor(..._: any[]) {
+//         super();
+//         if (st && id) {
+//           const h1Tag = document.getElementById(id)!;
+//           h1Tag.innerHTML = this.name;
+//         }
+//         // console.log(constructor);
+//         console.log("inside Deco factory" + st);
+//       }
+//     };
+//   };
+// };
+// //Property Decorator | Accessor decorators | Method decorators | Parameter decorators
+// @DecFactory("<span>Ali</span>", "test")
+// @SecondLogger
+// @Logger
+// class Student {
+//   name: string;
+//   constructor(name: string) {
+//     this.name = name;
+//     console.log("Creating student...");
+//   }
+// }
+// const newStudent = new Student("Hasan");
+// console.log(newStudent);
 
-class Product {
-  @Log1
-  name: string;
-  private _price: number;
+// const Log1 = (target: any, name: string | Symbol) => {
+//   console.log("Property decorator...");
+//   console.log(target);
+//   console.log(name);
+// };
+// const Log2 = (target: any, name: string, decorator: PropertyDescriptor) => {
+//   console.log("accessor decorator");
+//   console.log(target);
+//   console.log(name);
+//   console.log(decorator);
+// };
+// const Log3 = (
+//   target: any,
+//   name: string | Symbol,
+//   decorator: PropertyDescriptor
+// ) => {
+//   console.log("Method decorator.-.-.-.-.-.-.-");
+//   console.log(target);
+//   console.log(name);
+//   console.log(decorator);
+// };
+// const Log4 = (target: any, name: string | Symbol, position: number) => {
+//   console.log("Parameter decorator");
+//   console.log(target);
+//   console.log(name);
+//   console.log(position);
+// };
 
-  constructor(name: string, price: number) {
-    this.name = name;
-    this._price = price;
-  }
-  @Log2
-  set price(val: number) {
-    if (val > 0 && val) {
-      this._price = val;
-    } else {
-      throw new Error("Insert a posetive value!");
-    }
-  }
-  @Log3
-  createTax(@Log4 tax: number): number {
-    return this._price + tax;
-  }
-}
-const AutoBinder = (_: any, _2: string, descriptor: PropertyDescriptor) => {
-  const method = descriptor.value;
-  const newDescriptor: PropertyDescriptor = {
-    get() {
-      const boundFn = method.bind(this);
-      return boundFn;
-    },
-  };
-  return newDescriptor;
-};
+// class Product {
+//   @Log1
+//   name: string;
+//   private _price: number;
 
-class Printer {
-  message = "It works";
-  @AutoBinder
-  showPrint() {
-    console.log(this.message);
-  }
-}
-const newPrint = new Printer();
-const btn = document.querySelector("button")! as HTMLButtonElement;
-btn.addEventListener("click", newPrint.showPrint.bind(newPrint));
+//   constructor(name: string, price: number) {
+//     this.name = name;
+//     this._price = price;
+//   }
+//   @Log2
+//   set price(val: number) {
+//     if (val > 0 && val) {
+//       this._price = val;
+//     } else {
+//       throw new Error("Insert a posetive value!");
+//     }
+//   }
+//   @Log3
+//   createTax(@Log4 tax: number): number {
+//     return this._price + tax;
+//   }
+// }
+// const AutoBinder = (_: any, _2: string, descriptor: PropertyDescriptor) => {
+//   const method = descriptor.value;
+//   const newDescriptor: PropertyDescriptor = {
+//     get() {
+//       const boundFn = method.bind(this);
+//       return boundFn;
+//     },
+//   };
+//   return newDescriptor;
+// };
+
+// class Printer {
+//   message = "It works";
+//   @AutoBinder
+//   showPrint() {
+//     console.log(this.message);
+//   }
+// }
+// const newPrint = new Printer();
+// const btn = document.querySelector("button")! as HTMLButtonElement;
+// btn.addEventListener("click", newPrint.showPrint.bind(newPrint));
+
+// class Course {
+//   @Length(1, 10)
+//   title: string;
+//   @Min(2)
+//   @Max(8)
+//   task: number;
+//   constructor(title: string, task: number) {
+//     this.title = title;
+//     this.task = task;
+//   }
+// }
+// const form = document.getElementById("form")! as HTMLFormElement;
+// const title = document.getElementById("title")! as HTMLInputElement;
+// const task = document.getElementById("task")! as HTMLInputElement;
+
+// const validation = (course: {}) => {
+//   validate(course).then((err: any) => {
+//     if (err.length > 0) {
+//       console.log("Validation error", err);
+//     } else {
+//       console.log("Course created");
+//     }
+//   });
+// };
+
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   const newCourse = new Course(title.value, +task.value);
+//   validation(newCourse);
+// });
